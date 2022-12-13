034d41f72e492eaa2dd454c4469140d06acf8460
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2022-12-13 21:33:19 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,17 +1,17 @@
 d1 ={"name":"paritosh","name":"don", "class":"mca", "subject":"science"}
 print(d1)
-#print keys only
-#for i in d1:
-	#print(i)
-	#print values only
-	#print(d1[i])
-#print items(both)
+print keys only
+for i in d1:
+	print(i)
+	print values only
+	print(d1[i])
+print items(both)
 for i,j in d1.items():
 	print(i)
 	print(j)
-# print(d1.values())	
-# print(d1.items())
-# print(d1.keys())
+print(d1.values())	
+print(d1.items())
+print(d1.keys())
 d2 ={1:"21",2:"31",3:"41",3:"2121"}
 print(d2)
 for i,j in d2.items():
