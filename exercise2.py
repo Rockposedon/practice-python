f29b12bdcac5dedd03b1a464c6fac50a2d5cf5c9
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-01-22 10:14:35 +0530

    Add files via upload

---
+++ exercise2.py
@@ -0,0 +1,6 @@
+a = int(input("enter the number : "))
+condition = a%2
+if condition > 0:
+	print("number is odd")
+else:
+	print("number is even ")
---
+++ exercise3.py
@@ -0,0 +1,8 @@
+list1 = [1,2,3,4,5,6,7,8,2.3,2.3344,9.7]
+print(list1)
+# if list1.element>5:
+# 	print(list1)
+
+for i in list1:
+	if i < 5:
+		print(i)
