0f376ba5a75caf42d9e643459fac0f6f60f1142d
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-01-27 18:24:46 +0530

    Add files via upload

---
+++ 17(01).py
@@ -0,0 +1,19 @@
+# n = int(input(" enter value : "))
+# if n%4 == 0:
+#     print("Give year is leap year")
+#     if n%100 != 0:
+#         pass
+# else:
+#     if n%400 ==0 :
+#             print("Give year is leap year")
+#     else:
+#             print("not a leap year")
+n = int(input(" enter value : "))
+if n%400 == 0 and n%100 == 0 :
+    print("year is leap year")
+elif n%4 == 0 and n%100 != 0:
+    print("year is leap year")
+else :
+    print("year is not a leap year")
+
+
---
+++ 17-(01-02).py
@@ -0,0 +1,4 @@
+n = int(input("enter number : "))
+for i in range(1,11):
+    result = i*n
+    print(n , " X", i, "=", result)
