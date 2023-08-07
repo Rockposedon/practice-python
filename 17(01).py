9f76c953f5f2492a25815d00a8d8841b0f816523
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-07 15:57:03 +0530

    Update 17(01).py

--- 17(01).py
+++ 17(01).py
@@ -1,13 +1,16 @@
-# n = int(input(" enter value : "))
-# if n%4 == 0:
-#     print("Give year is leap year")
-#     if n%100 != 0:
-#         pass
-# else:
-#     if n%400 ==0 :
-#             print("Give year is leap year")
-#     else:
-#             print("not a leap year")
+# First approach to finding the leap year
+n = int(input(" enter value : "))
+if n%4 == 0:
+    print("Give year is leap year")
+    if n%100 != 0:
+        pass
+else:
+    if n%400 ==0 :
+            print("Give year is leap year")
+    else:
+            print("not a leap year")
+
+# Another approach to finding the leap year
 n = int(input(" enter value : "))
 if n%400 == 0 and n%100 == 0 :
     print("year is leap year")
