688d689d1d9a22c73b0d3eb15a1521c420cf9110
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 19:26:08 +0530

    multiple arguments addition

--- hacker-1.py
+++ hacker-1.py
@@ -1,4 +1,9 @@
-def f(a,b,c=1,d=5):
-    return a+b+c+d
-value = f(1,2,d=7)
-print(value)
+def addition(a, b, c=1, d=5):
+    # The function 'addition' takes four arguments: 'a', 'b', 'c', and 'd'.
+    # 'a' and 'b' are required arguments, while 'c' and 'd' are optional with default values.
+    
+    return a + b + c + d
+    # The function returns the sum of 'a', 'b', 'c', and 'd'.
+
+value = addition(1, 2, d=7)
+# Calling the function 'addition' with arguments '1', '2', and keyword argument 'd' set to '7'.
