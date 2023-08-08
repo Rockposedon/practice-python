e2bfabf9f999f83b996d1b87d8bd72a3349c8f30
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 17:00:58 +0530

    Python script to demonstrates various operations on dictionaries

--- dictionary.py
+++ dictionary.py
@@ -1,26 +1,57 @@
-d1 ={"name":"paritosh","name":"don", "class":"mca", "subject":"science"}
+'''
+	This Python script to demonstrates various operations on dictionaries, including adding, modifying,
+ 	and deleting key-value pairs, as well as access keys, values, and items in a dictionary
+'''
+# Define a dictionary d1 with key-value pairs
+d1 = {"name": "paritosh", "name": "don", "class": "mca", "subject": "science"}
+
+# Print the dictionary (Note: Duplicate keys will be overridden)
 print(d1)
-print keys only
+
+# Print keys only using a for loop
+print("Keys only:")
 for i in d1:
-	print(i)
-	print values only
-	print(d1[i])
-print items(both)
-for i,j in d1.items():
-	print(i)
-	print(j)
-print(d1.values())	
-print(d1.items())
-print(d1.keys())
-d2 ={1:"21",2:"31",3:"41",3:"2121"}
-print(d2)
-for i,j in d2.items():
-	print(i,j)
-d1["name"]="paritosh verma"
-print(d1)	
-d1["school"]="TKS"
-print(d1)
-#delete values using there key
+    print(i)
+
+# Print values only using a for loop
+print("Values only:")
+for i in d1:
+    print(d1[i])
+
+# Print items (key-value pairs) using a for loop
+print("Items (both):")
+for i, j in d1.items():
+    print(i)
+    print(j)
+
+# Print values using the values() method
+print("Values:", d1.values())
+
+# Print items using the items() method
+print("Items:", d1.items())
+
+# Print keys using the keys() method
+print("Keys:", d1.keys())
+
+# Define another dictionary d2 with key-value pairs (Note: Duplicate keys will be overridden)
+d2 = {1: "21", 2: "31", 3: "41", 3: "2121"}
+
+# Print key-value pairs using a for loop
+print("Key-value pairs in d2:")
+for i, j in d2.items():
+    print(i, j)
+
+# Modify the value associated with the "name" key
+d1["name"] = "paritosh verma"
+print("Modified d1:", d1)
+
+# Add a new key-value pair to d1
+d1["school"] = "TKS"
+print("Updated d1:", d1)
+
+# Delete a value using its key using the pop() method
 d1.pop("school")
-print(d1)
-d1.remove()
+print("After deleting 'school':", d1)
+
+# The line 'd1.remove()' is incorrect and will result in an AttributeError
+# There is no 'remove' method for dictionaries
