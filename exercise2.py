88fb7883cc1baae3aef0610f6690f5f446056e8b
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 17:06:49 +0530

    find the given number is odd or even

--- exercise2.py
+++ exercise2.py
@@ -1,6 +1,14 @@
+'''
+	find the given number is odd or even	
+'''
+# Prompt the user to enter a number and convert the input to an integer
 a = int(input("enter the number : "))
-condition = a%2
+
+# Calculate the remainder when 'a' is divided by 2
+condition = a % 2
+
+# Check if the remainder is greater than 0
 if condition > 0:
-	print("number is odd")
+    print("number is odd")
 else:
-	print("number is even ")
+    print("number is even")
