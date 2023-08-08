fff423ba02a6b07b050890fc80b099ea6eeff208
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-09 00:09:55 +0530

    swapping of variables

--- swapping.py
+++ swapping.py
@@ -1,6 +1,6 @@
-#swapping of program
-#in three wayss
-#taking addition variable
+# swapping of variables 
+# in three different ways
+# taking additional variable
 v1 = 1
 v2 = 20
 temp = v1
@@ -8,8 +8,8 @@ v1 = v2
 v2 = temp
 print("v1,v2,temp = ", v1,v2,temp)
 print("----------------------------------------------")
-# another way to solve 
-#without takking addition
+# Another way to solve 
+# without taking addition
 a = 1
 b = 20
 print(a,b)
@@ -19,8 +19,8 @@ a=a-b
 print(a,b)
 print("-----------------------------------------------")
 
-#another way
-#python special
+# another way
+# python special
 c = 20
 d = 90
 print(c,d)
