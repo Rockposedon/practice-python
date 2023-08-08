007e53f4d5a4d23fc49515f529d54b25cee2e83b
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 15:40:20 +0530

    Added problem statement 17-(01-02).py

--- 17-(01-02).py
+++ 17-(01-02).py
@@ -1,4 +1,14 @@
+''' problem statement
+    Write a Python script to generate the multiplication table of prompt-defined number
+'''
+
+# Take user input for the number to generate a multiplication table for
 n = int(input("enter number : "))
-for i in range(1,11):
-    result = i*n
-    print(n , " X", i, "=", result)
+
+# Iterate through numbers 1 to 10 to generate the multiplication table
+for i in range(1, 11):
+    # Calculate the result of the multiplication
+    result = i * n
+    
+    # Print the multiplication equation with comments
+    print(n, " X", i, "=", result)
