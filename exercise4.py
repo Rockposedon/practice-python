806f83ba2cd9c373603bfce31787e5f783cd1804
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 17:13:32 +0530

    Python script to find all the divisors of 'x'

--- exercise4.py
+++ exercise4.py
@@ -1,8 +1,21 @@
+'''
+	Python script to find all the divisors of 'x'
+'''
+# Prompt the user to enter a number and convert the input to an integer
 x = int(input("Enter a number: "))
-div=[]
-for y in range(1,x+1):
-	a=int(x/y)
-if x-a*y==0:
-	div.append(y)
 
+# Create an empty list to store divisors
+div = []
+
+# Iterate through numbers from 1 to x (inclusive)
+for y in range(1, x+1):
+    # Calculate the integer division of x by y
+    a = int(x / y)
+    
+    # Check if x is evenly divisible by y (no remainder)
+    if x - a * y == 0:
+        # If evenly divisible, add y to the 'div' list
+        div.append(y)
+
+# Print the list of divisors
 print(div)
