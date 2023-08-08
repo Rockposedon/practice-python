0ce1c5c90e4147eecaf02b76c5f8f153272d7959
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 09:45:32 +0530

    Update 17(01).py

--- 17(01).py
+++ 17(01).py
@@ -1,22 +1,31 @@
-# First approach to finding the leap year
-n = int(input(" enter value : "))
-if n%4 == 0:
-    print("Give year is leap year")
-    if n%100 != 0:
-        pass
-else:
-    if n%400 ==0 :
-            print("Give year is leap year")
+# First approach to determine if a year is a leap year or not
+n = int(input("Enter a year: "))  # Prompt the user to input a year
+
+# Check if the year is divisible by 4
+if n % 4 == 0:
+    print("Year is a leap year")  # Print a message if the year is divisible by 4
+    # Check if the year is not divisible by 100
+    if n % 100 != 0:
+        pass  # No further action is needed for this case
     else:
-            print("not a leap year")
-
-# Another approach to finding the leap year
-n = int(input(" enter value : "))
-if n%400 == 0 and n%100 == 0 :
-    print("year is leap year")
-elif n%4 == 0 and n%100 != 0:
-    print("year is leap year")
-else :
-    print("year is not a leap year")
+        # Check if the year is divisible by 400
+        if n % 400 == 0:
+            print("Year is a leap year")  # Print a message if the year is divisible by 400
+        else:
+            print("Year is not a leap year")  # Print a message if the year is not divisible by 400
+else:
+    print("Year is not a leap year")  # Print a message if the year is not divisible by 4
 
 
+
+# Another approach to determine if a year is a leap year or not
+n = int(input("Enter a year: "))  # Prompt the user to input a year
+
+# Check if the year is divisible by 400 and not divisible by 100
+if n % 400 == 0 and n % 100 != 0:
+    print("Year is a leap year")  # Print a message if the year meets the condition
+# Check if the year is divisible by 4 and not divisible by 100
+elif n % 4 == 0 and n % 100 != 0:
+    print("Year is a leap year")  # Print a message if the year meets the condition
+else:
+    print("Year is not a leap year")  # Print a message if the year doesn't meet any condition
