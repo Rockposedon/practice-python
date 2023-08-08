952f9571aba9c5e872c58d22b3b211a6b1242a21
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 17:24:21 +0530

    Fetch even elements from the list and print even list

--- exercise7.py
+++ exercise7.py
@@ -1,3 +1,11 @@
+'''
+      Fetch even elements from the list and print even list
+'''
+# Given a list of numbers
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-l1=list(i for i in a if i % 2 == 0)
+
+# Create a new list 'l1' using a list comprehension
+l1 = [i for i in a if i % 2 == 0]
+
+# Print the new list containing even numbers
 print(l1)
