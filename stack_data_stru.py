9415444080530b627a8e31963b46ff2058096674
paritosh <paritoshrock666666@gmail.com>
2023-12-24 12:49:32 +0530

    stack operations

--- stack_data_stru.py
+++ stack_data_stru.py
@@ -1,40 +1,52 @@
 from collections import deque
 
-class stack :
+class Stack:
     def __init__(self) -> None:
+        # Initialize the stack using a deque container
         self.container = deque()
-        
 
-    def push(self,val):
+    def push(self, val):
+        # Push the value onto the stack
         self.container.append(val)
-    
+
     def peek(self):
+        # Return the top element of the stack without removing it
         return self.container[-1]
-    
+
     def pop(self):
+        # Pop and return the top element of the stack
         if len(self.container) == 0:
             print("stack is empty")
-        else :    
+        else:
             return self.container.pop()
-    
+
     def size(self):
+        # Return the size of the stack
         if len(self.container) == 0:
             raise Exception("Stack is empty")
         else:
             return len(self.container)
 
-    def p(self):
+    def print_stack(self):
+        # Return the elements of the stack
         return self.container
 
+# Get input from the user
+str_input = input("Enter the string: ")
 
-str = input("enter the string:")
-my_stack = stack()
+# Create a stack instance
+my_stack = Stack()
 
-for char in str:
+# Push each character of the input string onto the stack
+for char in str_input:
     my_stack.push(char)
 
-size = my_stack.size()
-print(size)
+# Get the size of the stack
+size_of_stack = my_stack.size()
 
-for i in range(size):
+# Print the size of the stack
+print("Size of the stack:", size_of_stack)
+
+# Pop and print each element of the stack
+for i in range(size_of_stack):
     print(my_stack.pop())
